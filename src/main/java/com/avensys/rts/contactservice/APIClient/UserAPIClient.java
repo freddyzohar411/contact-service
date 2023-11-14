fa9f90011248318b0da84e237374d63e13c3abb4
hxg-avensys <H.Xiang@aven-sys.com>
2023-11-14 12:27:02 +0800

    Updated

--- src/main/java/com/avensys/rts/contactservice/APIClient/UserAPIClient.java
+++ src/main/java/com/avensys/rts/contactservice/APIClient/UserAPIClient.java
@@ -13,9 +13,6 @@ public interface UserAPIClient {
     @GetMapping("/{id}")
     HttpResponse getUserById(@PathVariable("id") Integer id);
 
-//    @GetMapping("")
-//    HttpResponse getUserByEmail(@RequestParam("email") String email);
-
     @GetMapping("/email/{email}")
     HttpResponse getUserByEmail(@PathVariable("email") String email);
 

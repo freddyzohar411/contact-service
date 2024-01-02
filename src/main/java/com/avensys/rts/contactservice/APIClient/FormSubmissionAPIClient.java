6dc7131da45a4d357005952060234420a3e33fc1
hxg-avensys <H.Xiang@aven-sys.com>
2024-01-02 15:41:07 +0800

    Updated

--- src/main/java/com/avensys/rts/contactservice/APIClient/FormSubmissionAPIClient.java
+++ src/main/java/com/avensys/rts/contactservice/APIClient/FormSubmissionAPIClient.java
@@ -9,17 +9,17 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "form-service", url = "${api.form-submission.url}", configuration = JwtTokenInterceptor.class)
 public interface FormSubmissionAPIClient {
 
-	@PostMapping("/form-submissions")
+	@PostMapping("")
 	HttpResponse addFormSubmission(@RequestBody FormSubmissionsRequestDTO formSubmissionsRequestDTO);
 
-	@GetMapping("/form-submissions/{formSubmissionId}")
+	@GetMapping("/{formSubmissionId}")
 	HttpResponse getFormSubmission(@PathVariable Long formSubmissionId);
 
-	@PutMapping("/form-submissions/{formSubmissionId}")
+	@PutMapping("/{formSubmissionId}")
 	HttpResponse updateFormSubmission(@PathVariable Long formSubmissionId,
 			@RequestBody FormSubmissionsRequestDTO formSubmissionsRequestDTO);
 
-	@DeleteMapping("/form-submissions/{formSubmissionId}")
+	@DeleteMapping("/{formSubmissionId}")
 	HttpResponse deleteFormSubmission(@PathVariable Long formSubmissionId);
 
 }

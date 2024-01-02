6d09794a7b195305cb056cf0979a38d1cf1654a5
hxg-avensys <H.Xiang@aven-sys.com>
2024-01-02 15:32:36 +0800

    Updated add api endpoint

--- src/main/java/com/avensys/rts/contactservice/controller/ContactController.java
+++ src/main/java/com/avensys/rts/contactservice/controller/ContactController.java
@@ -7,15 +7,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.avensys.rts.contactservice.constant.MessageConstants;
 import com.avensys.rts.contactservice.payloadnewrequest.ContactRequestDTO;
@@ -27,6 +19,7 @@ import com.avensys.rts.contactservice.util.ResponseUtil;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/api/contacts")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ContactController {
 
@@ -42,7 +35,7 @@ public class ContactController {
 		this.messageSource = messageSource;
 	}
 
-	@PostMapping("/contacts")
+	@PostMapping("")
 	public ResponseEntity<Object> createContact(@Valid @RequestBody ContactRequestDTO contactRequest,
 			@RequestHeader(name = "Authorization") String token) {
 		log.info("Create a contact : Controller ");
@@ -54,7 +47,7 @@ public class ContactController {
 				messageSource.getMessage(MessageConstants.MESSAGE_CREATED, null, LocaleContextHolder.getLocale()));
 	}
 
-	@GetMapping("/contacts/entity/{entityType}/{entityId}")
+	@GetMapping("/entity/{entityType}/{entityId}")
 	public ResponseEntity<Object> getContactsByEntityTypeAndEntityId(@PathVariable String entityType,
 			@PathVariable Integer entityId) {
 		log.info("Get contacts by entity type and entity id : Controller ");
@@ -63,7 +56,7 @@ public class ContactController {
 				messageSource.getMessage(MessageConstants.MESSAGE_SUCCESS, null, LocaleContextHolder.getLocale()));
 	}
 
-	@DeleteMapping("/contacts/{id}")
+	@DeleteMapping("/{id}")
 	public ResponseEntity<Object> deleteContact(@PathVariable Long id) {
 		log.info("Delete contact : Controller ");
 		contactService.deleteContact(id);
@@ -71,7 +64,7 @@ public class ContactController {
 				messageSource.getMessage(MessageConstants.MESSAGE_SUCCESS, null, LocaleContextHolder.getLocale()));
 	}
 
-	@PutMapping("/contacts/{id}")
+	@PutMapping("/{id}")
 	public ResponseEntity<Object> updateContact(@PathVariable Long id,
 			@Valid @RequestBody ContactRequestDTO contactRequest,
 			@RequestHeader(name = "Authorization") String token) {
@@ -90,7 +83,7 @@ public class ContactController {
 	 * @param entityId
 	 * @return
 	 */
-	@DeleteMapping("/contacts/entity/{entityType}/{entityId}")
+	@DeleteMapping("/entity/{entityType}/{entityId}")
 	public ResponseEntity<Object> deleteContactsByEntityTypeAndEntityId(@PathVariable String entityType,
 			@PathVariable Integer entityId) {
 		log.info("Delete contacts by entity type and entity id : Controller ");

f65a0f5458d8227deb6179021b95ccf8f4905f88
rku-avensys <rahul@aven-sys.com>
2024-02-23 16:56:53 +0530

    removed @data annotation

--- src/main/java/com/avensys/rts/contactservice/payloadnewrequest/FormSubmissionsRequestDTO.java
+++ src/main/java/com/avensys/rts/contactservice/payloadnewrequest/FormSubmissionsRequestDTO.java
@@ -3,10 +3,12 @@ package com.avensys.rts.contactservice.payloadnewrequest;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class FormSubmissionsRequestDTO {
--- src/main/java/com/avensys/rts/contactservice/payloadnewresponse/UserResponseDTO.java
+++ src/main/java/com/avensys/rts/contactservice/payloadnewresponse/UserResponseDTO.java
@@ -1,31 +1,32 @@
 package com.avensys.rts.contactservice.payloadnewresponse;
 
-import com.avensys.rts.contactservice.payloadnewrequest.user.UserGroupResponseDTO;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 import java.util.List;
+
+import com.avensys.rts.contactservice.payloadnewrequest.user.UserGroupResponseDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * UserResponseDTO
  */
-@Data
+@Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 //@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserResponseDTO {
-    private Long id;
-    private String keycloackId;
-    private String firstName;
-    private String lastName;
-    private String username;
-    private String email;
-    private String mobile;
-    private String employeeId;
-    private Boolean locked;
-    private Boolean enabled;
-    private List<UserGroupResponseDTO> userGroup;
+	private Long id;
+	private String keycloackId;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String email;
+	private String mobile;
+	private String employeeId;
+	private Boolean locked;
+	private Boolean enabled;
+	private List<UserGroupResponseDTO> userGroup;
 }
